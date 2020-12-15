1fa4b8f4992c49db5dacb0f1736dd170704e1bff
Therbin <theo.herbin59710@outlook.fr>
2020-12-15 08:31:00 +0000

    e

--- src/Entity/ListeDeMots.php
+++ src/Entity/ListeDeMots.php
@@ -6,9 +6,11 @@ use App\Repository\ListeDeMotsRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiResource;
 
 /**
  * @ORM\Entity(repositoryClass=ListeDeMotsRepository::class)
+ * @ApiResource()
  */
 class ListeDeMots
 {
--- src/Entity/Tests.php
+++ src/Entity/Tests.php
@@ -6,9 +6,11 @@ use App\Repository\TestsRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiResource;
 
 /**
  * @ORM\Entity(repositoryClass=TestsRepository::class)
+ * @ApiResource()
  */
 class Tests
 {
